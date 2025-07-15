45064d060639bafe271a78503b3d612f176cb8e5
özgür aykutalp <prof1998@hotmail.com>
2025-07-15 19:09:17 +0300

    Bugfix

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,7 @@
 
 import HeroSection from './components/HeroSection';
 import SystemArchitecture from './components/SystemArchitecture';
-import TechnicalMetrics from './components/TechnicalMetrics';
 import ContactSection from './components/ContactSection';
-import AboutMe from './components/AboutMe';
 import WorkExperience from './components/WorkExperience';
 
 export default function BackendPortfolio() {
